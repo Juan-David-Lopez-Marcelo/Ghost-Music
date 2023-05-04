923b2e7e4accc610c051cfecd05dca70e1005aab
Juan Lopez <jdavidxlopez@gmail.com>
2023-05-04 18:47:04 -0500

    Creacion de paquetes y clases de la vista

---
+++ src/ghost/music/GhostMusic.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ghost.music;
+
+/**
+ *
+ * @author Usuario
+ */
+public class GhostMusic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
